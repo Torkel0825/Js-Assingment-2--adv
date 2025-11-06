fae68a4df2394ae0c1afa09862a8753947fbe754
Torkel0825 <dtk000825@gmail.com>
2025-11-06 13:23:04 +0100

    new file, needs implementing

---
+++ addBook.js
@@ -0,0 +1,5 @@
+// constants
+const addBook = document.getElementById("add-book");
+const bookStorage = append("./bookStorage.json"); // don't know if it will work yet
+
+// storing book in local storage( in bookStorage.json )
